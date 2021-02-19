13b1258f309901332a55b324a2dc92dd99635e82
Artem Akopian <artem7498@gmail.com>
2021-02-19 16:38:21 +1000

    congrats vc added

---
+++ Skillbox 2.11/CongratsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CongratsViewController.swift
+//  Skillbox 2.11
+//
+//  Created by Артём on 2/19/21.
+//
+
+import UIKit
+
+class CongratsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
